a22f5c5e3f04ee38f614ddad310e226f88feefd7
Gaasu <gaasu.bwajteam@gmail.com>
2018-06-19 21:09:59 +0700

    Update 19/06 Finish

--- Task3/Bai4/bai4.py
+++ Task3/Bai4/bai4.py
@@ -3,69 +3,95 @@ import numpy as np
 from collections import defaultdict
 from mpl_toolkits.mplot3d import Axes3D
 import math
+import datetime
 
+# Chuyen tu unixTime ve readData time
+def conventUnixTime(unixTime):
+    return datetime.datetime.fromtimestamp(
+        float(unixTime)
+    ).strftime('%Y-%m-%d')
+
+# Đọc dữ liệu từ file, đưa các con ngựa vào từng dict (id , list tọa độ)
 def readData(fileName):
     res = defaultdict(list)
     with open(fileName) as f:
         for x in f:
-            horse = x.split(",")        
+            horse = x.split(",")
+            horse[0] = conventUnixTime(horse[0])        
             idHorse = horse[3]                             
             res[idHorse].append(horse)
     return res
 
-
-list_horse = readData("ZebraBotswana.txt")    
-print(list_horse.keys()) 
-for keyName in list_horse.keys():
-    maxLen = 100000
-    t1, t2 =0, 0
-    for i in range(len(list_horse[keyName]) - 1):
-        x1 = float(list_horse[keyName][i][2])
-        y1 = float(list_horse[keyName][i][1])  
-        tmpt1 = float(list_horse[keyName][i][0])
-               
-        x2 = float(list_horse[keyName][i+1][1])       
-        y2 = float(list_horse[keyName][i+1][2])   
-        tmpt2 = float(list_horse[keyName][i+1][0])    
-
-        tempLen = float(math.hypot(x2 - x1, y2 - y1))
-        # deltaTime = (t2 - t1) / 86400 
-        # Cach 24h
-
-        # if(tempLen < maxLen and deltaTime >= 1): 
-        if(tempLen < maxLen): 
-            maxLen = tempLen
-            t1 = tmpt1
-            t2 = tmpt2
-            
-    print("Hai thời điểm mà",keyName,"có khoảng cách nhỏ nhất: ",t1,t2)
+# Tinh khoang cach gan nhat giua 2 ngay bat ki
+def minIn2Day(day1, day2):
+    res = 1000000000
+    for data1 in day1:
+        for data2 in day2:
+            resTemp = float(math.hypot(float(data2[0]) - float(data1[0]), float(data2[1]) - float(data1[1])))
+            if(resTemp < res): res = resTemp
+    return res
 
 
 
 
 
-# print(list_horse.keys())
-# print(list_horse['Z3864\n'][-1][0])
-fig = plt.figure()
-ax = fig.add_subplot(111, projection = "3d")
-x, y, z = [], [], []
+# Tìm 2 ngày mà có khoảng cách ngắn nhất
+def minOfHorse(horse):
+    res = 1000000000
+    day1, day2 = 0, 0
+    key = list(horse.keys())
+    for i in range(len(key)- 1):
+        day1 = key[i]
+        for j in range(1, len(key)):       
+            day2 = key[j]
+            resTemp = minIn2Day(horse[day1], horse[day2])
+            if(resTemp < res): 
+                res = resTemp
+    return res, day1, day2     
+
+
+
+
+
+# Tính khoảng cách của từng con ngựa
+list_horse = readData("ZebraBotswana.txt")  
+# print(list_horse['Z6399\n']) 
+for key in list_horse:
+    res = defaultdict(list)
+    for horse in list_horse[key]:  
+        viTri = [horse[1], horse[2]]
+        time = horse[0]
+        res[time].append(viTri)
+
+    lengh, day1, day2 = minOfHorse(res)
+    print("2 ngay co khoang cach gan nhau nhat cua id", key, "la ngay ", day1 , " va ngay ", day2, "\n",
+    "Khoảng cách ngắn nhất là", lengh)
+
+
+
+
+# Vẽ đường đi của ngựa
+
 
 color = ['r','b','g','k','c','y','m']
+index = -1
 for key in list_horse.keys():
-    index = -1
+    x, y = [], []
+    index += 1
+    c =  color[index] 
     for i in list_horse[key]:
         x.append(float(i[1]))
-        y.append(float(i[2]))
-        z.append(float(i[0]))
-        
-    ax.scatter(x, y, z, c= color[index + 1], marker='o', label =key)
+        y.append(float(i[2]))    
+    sorted(x)
+    sorted(y)
+    plt.plot(x, y, c = c, label = key)
     
 plt.legend()
-ax.set_xlabel('Vĩ độ')
-ax.set_ylabel('Kinh độ')
-ax.set_zlabel('Thời gian')
-plt.savefig("horse.png")
-plt.show()
+plt.xlabel('Vĩ độ')
+plt.ylabel('Kinh độ')
+plt.savefig("horse.png")  
+# plt.show()
+
         
 
 
