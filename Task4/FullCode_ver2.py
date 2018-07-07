e88408c531e3723a7dec2fd6b971db0acbfa147b
bwajteam <gaasu.bwajteam@gmail.com>
2018-07-06 19:53:16 -0700

    update 07/07

--- Task4/FullCode_ver2.py
+++ Task4/FullCode_ver2.py
@@ -111,26 +111,43 @@ if res != None:
 print("1.5")
 day1 = '1988-06-25'
 day2 = '1989-06-25'
-
+#  tinh luong nhan vien
 query = "\
 SELECT departments.dept_name, SUM(salaries.salary) \
-FROM departments, dept_emp, dept_manager, salaries \
-WHERE dept_emp.dept_no = departments.dept_no \
-    AND dept_manager.dept_no = departments.dept_no \
-    AND dept_emp.emp_no = salaries.emp_no \
-    AND dept_manager.emp_no = salaries.emp_no \
-    AND salaries.from_date between '%s' AND '%s' \
-    AND salaries.to_date between '%s' AND '%s' \
-    AND dept_emp.from_date <= salaries.from_date \
-    AND dept_emp.to_date >= salaries.to_date \
-    AND dept_manager.from_date <= salaries.from_date \
-    AND dept_manager.to_date >= salaries.to_date \
-GROUP BY departments.dept_no \
-" % (day1, day2, day1, day2)
+FROM salaries, dept_emp, departments \
+WHERE salaries.from_date >= '%s' \
+AND salaries.to_date <= '%s' \
+AND salaries.emp_no = dept_emp.emp_no \
+AND departments.dept_no = dept_emp.dept_no \
+GROUP BY departments.dept_name \
+" % (day1, day2)
 cur.execute(query)
 res = cur.fetchall()
-print("Tổng lương phải trả của mỗi phòng ban trong khoản thời gian from_date = %s và to_date %s" %(day1, day2))
+
+salaries = []
 for data in res:
+    salaries[data[0] = data[0][0]
+
+# Tinh luong manager neu manager chua tung la nhan vien
+
+query = "\
+SELECT departments.dept_name, salaries.salary, dept_manager.emp_no, salaries.from_date\
+FROM salaries, dept_manager, departments\
+WHERE salaries.from_date >= '%s'\
+AND salaries.to_date <= '%s'\
+AND dept_manager.emp_no = salaries.emp_no\
+AND departments.dept_no = dept_manager.dept_no\
+AND dept_manager.dept_no NOT IN (SELECT dept_emp.emp_no FROM dept_emp)\
+" %(day1, day2)
+
+cur.execute(query)
+res = cur.fetchall()
+
+for data in res:
+    salaries[data[0] += data[0][0]
+
+print("Tổng lương phải trả của mỗi phòng ban trong khoản thời gian from_date = %s và to_date %s" %(day1, day2))
+for data in salaries:
     print("Phong ban: %s tra %d" % (data[0], data[1]))
 
 # 2.1
