036ab427ae632c022e4b945a68d44c8f1a16963c
bwajteam <gaasu.bwajteam@gmail.com>
2018-09-10 15:31:19 +0700

    update final

--- Task6/README.txt
+++
@@ -1,6 +0,0 @@
-﻿Task 6 Recomment Film
-Ý tưởng thực hiện bài toán: Sử dụng giải thuật lọc cộng tác dựa trên items. Các bước thực hiện: 
-	+Vì input là các bộ phim mà user đó thích, cho nên, ta tìm tất cả những người dùng cũng thích các phim mà user thích bằng cách từ file ratings, chọn ra những rating có id phim = id phim input, rating >= 3 (hoặc rating >= rating của input- trong bài t chọn >= 3) 
-	+Từ danh sách trên, lọc ra những người dùng có số đánh giá tương ứng với input là cao nhất (t chọn tỉ lệ 80% đánh giá thích giống input) 
-	+Sau khi chọn được những người có đánh giá tương đồng input, bắt đầu chọn ra tất cả các phim mà những người đó cũng thích (rating >=3 tiếp). 
-	+Sau khi có danh sách phim ưa thích của những người đó, ta chọn ra những phim mà có số người thích cao nhất và có đánh giá trung bình cũng cao, ở đây, t chọn ra các phim có 80 số người thích và ratings >= 4. Kết quả thu được sẽ là những phim mà input có thể sẽ thích.
--- Task6/slover.py
+++ Task6/slover.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python
 
 import csv
-# import numpy as np
 
-
-
-# Lấy ra userId,movieId,rating trong file "ratings" thõa mãn movieId = input_movieId và rating = input_rating
-def slove():
-    ratings_file = "ratings_real.csv"
-    min_film_rating_counts_percent = 0.6 #Số bộ fiml đánh giá giống nhau giữa input.csv và rating.csv
+def slove(ratings_file_name):
+    
+    min_film_rating_counts_percent = 0.6 #Số bộ phim đánh giá giống nhau giữa input.csv và rating.csv
     min_user_count_rating_percent = 0.6 #Số lượng người đánh giá tối thiểu
     number_of_recomment_films = 5
-    ratings_data = open(ratings_file, "r").readlines()[1:]
+    ratings_data = open(ratings_file_name, "r").readlines()[1:]
 
-    input_user = []
-    input_movie = []
+    input_user = [] # Lưu userId đang xét
+    input_movie = [] # Lưu danh sách movieId user đó đã xem
     input_movie_and_ratings = [] # [movieId][rating] của input
 
     for ln in input_data:
@@ -23,29 +19,18 @@ def slove():
         if ln[0] not in input_user:
             input_user.append(ln[0])
     output_list_ln.append(input_user[0])
-    # print("Input user len = ", len(input_user))
-    # print(input_user)
 
-    # print("Input movies len = ", len(input_movie))
-    # print(input_movie)
-
-    # print("input movies and ratings len ", len(input_movie_and_ratings))
-    # print(input_movie_and_ratings)
-
-
+# Lưu các ratings_data trong file rating vào s1.csv nếu các data đó thỏa mãn điều kiện
     with open("s1.csv", "w") as s1:
         for ln in ratings_data:
             ln_temp = ln.split(",")
-            if ln_temp[1] in input_movie:
-                if (str(ln_temp[0]) not in input_user and str(ln_temp[1]) in input_movie and float(ln_temp[2]) > 3 ):
-                    s1.write(ln)
+            if (str(ln_temp[0]) not in input_user and str(ln_temp[1]) in input_movie and float(ln_temp[2]) > 3 ):
+                s1.write(ln)
     s1.close()
 
-    # Lấy ra danh sách userId, filmRatingCounts với filmRatingCounts là số bộ film có cùng đánh giá với input_user.
-    # filmRatingCounts >= min_film_rating_counts (số bộ phim có cùng đánh giá tối thiểu tối thiểu)
 
     s1_data = open("s1.csv", "r").readlines()
-    s2 = open("s2.csv", "w")
+    
     movies_counts_ratings = {} #movies and ratings của input user
     recomment_user = [] # Danh sách user để gợi ý
     for n in range(0, len(s1_data)):
@@ -56,18 +41,12 @@ def slove():
         else:
             movies_counts_ratings[key] = 1
 
-    # print("Movies and ratings count")
-    # print(movies_counts_ratings)
-
     max_film_rating_counts = len(input_movie)
     min_film_rating_counts = min_film_rating_counts_percent * max_film_rating_counts
 
-
-    output_list_ln.append(max_film_rating_counts)
-    output_list_ln.append(round(min_film_rating_counts, 1))
     
-
-
+# Lưu các userId và số bộ phim tương đồng với input ra file s2.csv
+    s2 = open("s2.csv", "w")
     for item in movies_counts_ratings.items():
         if item[1] >= min_film_rating_counts:
             s2.write(str(item[0])+ "," + str(item[1])+ "\n")
@@ -77,7 +56,7 @@ def slove():
 
 
 
-    # Lấy ra userId,movieId,rating trong file "ratings" thõa mãn userId = userId trong file s2 và movieId != input_movieId
+# Lưu các bộ phim có ratings > 3 của các user tương đồng ra file s3.csv
 
     s3 = open("s3.csv", "w")
     for ln in ratings_data:
@@ -90,12 +69,11 @@ def slove():
 
 
 
-    # Đưa ra movieId, số người đánh giá
+# Đưa ra movieId, số người đánh giá
 
     s3 = open("s3.csv", "r").readlines()
-    counts_dict = {} # key = movieId, item = count ratings
-    sum_dict = {} #key = moviedId, item = sum of ratings
-    # arv_dict = {} #key = moviedId, item = arv of ratings
+    counts_dict = {} # key = movieId, item = số người đánh giá
+    sum_dict = {} #key = moviedId, item = tổng điểm đánh giá
 
     for ln in s3:
         ln = ln.split(",")
@@ -111,42 +89,27 @@ def slove():
         else:
             sum_dict[count1] += float(ln[2].strip())
 
-    # Tính điểm đánh giá trung bình
-
-    # for id_movie in counts_dict.keys():
-    #     arv_temp = sum_dict[id_movie] / counts_dict[id_movie]
-    #     arv_dict[id_movie] = arv_temp
-
+# Sắp xếp theo số người đánh giá từ nhiều nhất đến it nhất
     counts_dict_sorted = sorted(counts_dict.items(),key = lambda arv: arv[1], reverse = True)
 
-
+# Ghi ra file s4.csv movieId và số người đánh giá theo thứ tự từ nhiều nhất -> ít nhất
     s4 = open("s4.csv", "w")
     for item in counts_dict_sorted:
-        # s4.write(str(item[0]) + "," + str(item[1]) + str(arv_dict[float(item[0])]) + "\n")
         s4.write(str(item[0]) + "," + str(item[1]) +  "\n")
     s4.close()
 
-    # print("Danh sách movies and ratings count")
-    # print(counts_dict)
+    recomment_user_len = len(recomment_user)
+    min_user_count_rating = recomment_user_len * min_user_count_rating_percent
 
-
-    # Đưa ra danh sách film gợi ý với điểm đánh giá trung bình (min_avg_rating) và số người đánh giá (min_user_count_rating) được xác định
-    user_lengh = len(recomment_user)
-    min_user_count_rating = user_lengh * min_user_count_rating_percent
-
-    output_list_ln.append(user_lengh)
-    output_list_ln.append(round(min_user_count_rating, 1))
-    # print("Số user tương đồng: ", user_lengh)
-    # print("Số user tương đồng tối thiểu: ", min_user_count_rating)
+    output_list_ln.append(recomment_user_len)
         
     movies = open("ml-movies.csv", encoding="utf8").readlines()[1:]
-
 
     with open("s5.csv", "w") as f:
         f.write(','.join(['movieId', 'title', 'genres', '\n']))
     f.close()
 
-    # Lấy ra number_of_recomment_films movies có số rating lớn nhất
+# Lấy ra number_of_recomment_films movies có số người đánh giá lớn nhất
     s5 = open("s5.csv", "a")
     count = 0
     for item in counts_dict.items():
@@ -154,8 +117,6 @@ def slove():
             count += 1
             movie_id = int(item[0])
             user_count_rating = int(item[1])
-            # print("movieId = ", movie_id)
-            # print("user count rating = ",user_count_rating)
             
             if user_count_rating >= min_user_count_rating:
                 for row in movies:
@@ -167,51 +128,59 @@ def slove():
     s5.close()
 
 
+# Hàm check thực hiện đếm số bộ phim gợi ý đúng trong danh sách phim gợi ý đưa ra ở slove()
+# Tỉ lệ = số bộ phim đúng/ tổng số phim
+# Tỉ tệ = 0 khi không gợi ý đúng 1 phim nào
+# Tỉ lệ = -1 khi không gợi ý phim nào
 def check(check_data, output_file):
-        check_input = check_data
         recomment_movies = open(output_file, "r").readlines()[1:]
-
         correct_movies = 0
         check_movies = []
-        correct_percent = -1
-        for ln in check_input:
-            check_movies.append(ln[1])
+        correct_percent = 1
+        if len(recomment_movies) > 0:           
+            for ln in check_data:
+                check_movies.append(ln[1])
 
-        for ln in recomment_movies:
-            ln = ln.split(",")
-            if ln[0] in check_movies:
-                correct_movies+= 1
-        if len(recomment_movies) > 0:
-            correct_percent = (correct_movies / len(recomment_movies))*100
+            for ln in recomment_movies:
+                ln = ln.split(",")
+                if ln[0] in check_movies:
+                    correct_movies+= 1
+            if len(recomment_movies) > 0:
+                correct_percent = (correct_movies / len(recomment_movies))*100
+  
+        else:
+            correct_movies = -1
+            correct_percent = -1
 
-        # print("Correct movies = " , correct_movies)
-        # print("Correct percent = ", correct_percent)  
+        output_list_ln.append(len(recomment_movies))
+        output_list_ln.append(correct_movies) 
+        output_list_ln.append(round(correct_percent, 1))
 
-        output_list_ln.append(correct_movies)  
-        output_list_ln.append(correct_percent)
-
- 
-
-# Main 
-
-ratings_data_first = open("ratings_real.csv", "r").readlines()[1:2]
+# ---Main--- 
+ratings_file_name = "ratings_test.csv"
+ratings_data_first = open(ratings_file_name, "r").readlines()[1:2]
 ratings_data_first_split = ratings_data_first[0].split(",")
-ratings_data = open("ratings_real.csv", "r").readlines()[2:]
+ratings_data = open(ratings_file_name, "r").readlines()[2:]
 
 
-input_data_new = []
+input_data_new = [] #Lưu tất cả ratings của từng user, reset khi đến userId mới
 input_data_new.append(ratings_data_first_split)
-input_data = []
-check_data = []
+input_data = [] #Lưu ratings của user để train
+check_data = [] #Lưu ratings của user để test
 
-output_list_ln = []
+
+output_list_ln = [] #Lưu output với format ['userId', 'movies_input_len', 'movies_like_input_min','user_same_rating','user_same_rating_min','correct_movies', 'correct_percent', '\n'
+
 with open("output.csv", "w") as f:
-        f.write(','.join(['userId', 'movies_input_len', 'movies_like_input_min','user_same_rating','user_same_rating_min','correct_movies', 'correct_percent', '\n']))
+        f.write(','.join(['userId','soUserTuongDong','soBoPhimGoiY','soPhimGoiYDung', 'xacSuat', '\n']))
 f.close()
 output_ratings = open("output.csv", "a")
 
+print(','.join(['userId','soUserTuongDong','soBoPhimGoiY','soPhimGoiYDung', 'xacSuat']))
+
 for ln in ratings_data:
     ln_split = ln.split(",")
+    # Lấy ratings_data của từng user. Nếu muốn gợi ý cho user tùy ý, có thể thêm bước check userId ở đây là được.
     if(ln_split[0] == ratings_data_first_split[0]):
         input_data_new.append(ln_split)
         
@@ -219,18 +188,23 @@ for ln in ratings_data:
         ratings_data_first = ln
         ratings_data_first_split = ratings_data_first.split(",")
         index = int(0.5* len(input_data_new))
+        # 1/2 data đưa vào train, 1/2 data đưa vào test
         for i in range(0, index):
             input_data.append(input_data_new[i])
         for i in range(index, len(input_data_new)):
             check_data.append(input_data_new[i])
 
-        slove()
+        # Train
+        slove(ratings_file_name)
 
-        out_file_name = "s5.csv"
-        check(check_data, out_file_name)
+        # Test
+        check(check_data, "s5.csv")
+
+        # Print
         print(output_list_ln)
         output_ratings.write(str(output_list_ln) + "\n")
 
+        # Reset lại giá trị cho userId mới.
         input_data_new = []
         input_data_new.append(ratings_data_first_split)
         input_data = []
